108c8a3d0f4566e4ce041a197290e60eb12e330e
m-stoev <mp.stoev@gmail.com>
2019-10-31 13:46:45 +0200

    Added available map markers.

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Miroslav PS Google Maps Plugin
+Plugin URI: https://github.com/m-stoev/mps-google-maps
+Description: Plugin for Google maps and POIs.
+Version: 0.1
+Author: Miroslav Stoev
+*/
+
+defined('ABSPATH') || die('die');
+
+add_action('plugins_loaded', 'mps_gm_init', 0);
+
+function mps_gm_init() {
+	
+}
