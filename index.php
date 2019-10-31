8c2754553da214614c7ba51d8f436c5f12be59fc
m-stoev <mp.stoev@gmail.com>
2019-10-31 14:01:27 +0200

    Added admin menu.

--- index.php
+++ index.php
@@ -9,8 +9,24 @@ Author: Miroslav Stoev
 
 defined('ABSPATH') || die('die');
 
-add_action('plugins_loaded', 'mps_gm_init', 0);
+add_action('plugins_loaded',	'mps_gm_init', 0);
+add_action('admin_menu',		'mps_add_plugin_menu');
 
 function mps_gm_init() {
+	
+}
+
+function mps_add_plugin_menu() {
+	add_menu_page(
+		'MPS Google Maps',
+		'MPS Google Maps',
+		'manage_options',
+		__FILE__,
+		'mps_render_page',
+		'dashicons-admin-site-alt'
+	);
+}
+
+function mps_render_page() {
 	
 }
